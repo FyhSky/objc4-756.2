12e24193ef61190b531134122e90b169b0064ae3
FyhSky <fengyinghao@qiyi.com>
2019-08-27 11:06:13 +0800

    Initialize project

---
+++ objc-debug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  objc-debug
+//
+//  Created by FengYinghao on 27/8/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSObject *object = [NSObject new];
+        
+        NSLog(@"Hello, World! %@",object);
+    }
+    return 0;
+}
